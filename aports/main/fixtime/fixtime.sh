c77a7b52b4f5c0cf50de33037cf26b6a553d1b1c
Pavel <pavel@ucw.cz>
2017-09-29 14:34:04 +0200

    Fixtime version that builds.

--- aports/main/fixtime/fixtime.sh
+++ aports/main/fixtime/fixtime.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
+
+### BEGIN INIT INFO
+# Provides:          fixtime
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# X-Interactive:     true
+# Short-Description: Fix time to be monotonic
+# Description:       On machines that forget time when battery is removed,
+#                    at least make sure time is monotonic.
+### END INIT INFO
+
 F=/etc/fixtime.time
-if [ $(date +%s) -lt $(< $F) ];
-    then
-    date -s @$(< $F)
-    fi
-date +%s > $F
+
+case "$1" in
+    start)
+	if [ $(date +%s) -lt $(< $F) ];
+	then
+	    date -s @$(< $F)
+	fi
+	date +%s > $F
+	;;
+    stop)
+	date +%s > $F
+	;;
+    esac
+
